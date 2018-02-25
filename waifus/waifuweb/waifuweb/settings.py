a39e3f012571370f273637155af20b352b39cc97
Anthony Mella <anthony@Anthonys-MacBook-Pro.local>
2018-02-24 21:15:20 -0500

    set up templates, and added bootstrap

--- waifus/waifuweb/waifuweb/settings.py
+++ waifus/waifuweb/waifuweb/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'waifuweb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'waifuvid/templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
